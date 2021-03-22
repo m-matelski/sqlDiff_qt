e3db220fd157f247f49b6323d8c9c6d6e8ca2e9f
Mateusz <m.z.matelski@gmail.com>
2021-03-22 21:17:53 +0100

    running ui main window

--- sqldiff/main.py
+++ sqldiff/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from sqldiff.ui.designer.main_window import Ui_MainWindow
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+app = QApplication(sys.argv)
+w = MainWindow()
+app.exec_()
