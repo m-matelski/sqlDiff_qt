3b471a090e39d70a8bde86a073e62a792d2a6eee
Mateusz <m.z.matelski@gmail.com>
2021-04-11 21:52:27 +0200

    added generating resource file. set to relative resource import in compiled ui scripts

--- pyuic_compile_design.sh
+++ pyuic_compile_design.sh
@@ -13,9 +13,12 @@ find "$root_ui" -type f -name "*.ui" | while read line; do
   echo "Processing file '$line'"
   current_file=${line#"$root_ui"}
   current_py_dir="$(dirname "$root_py$current_file")"
-  current_py_filepath="$current_py_dir/$(basename -s .ui "$current_file").py"
+  current_py_filepath="$current_py_dir/ui_$(basename -s .ui "$current_file").py"
   mkdir -p "$current_py_dir"
   touch "$current_py_filepath"
   touch "$current_py_dir/__init__.py"
-  pyuic5 "$line" -o "$current_py_filepath"
+  pyuic5 "$line" -o "$current_py_filepath" --resource-suffix=_rc --import-from=.
 done
+
+# Compile resource file "resources.qrc" if exists
+pyrcc5 "${root_ui}/resources.qrc" -o "${root_py}/resources_rc.py"
