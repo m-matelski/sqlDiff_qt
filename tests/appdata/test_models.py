7b36be6198881c4a117be6170bd9ffa99b642cc5
Mateusz <m.z.matelski@gmail.com>
2021-04-28 18:46:25 +0200

    removed tests

--- tests/appdata/test_models.py
+++ tests/appdata/test_models.py
@@ -1,40 +1,8 @@
 import unittest
-from pathlib import Path
-from uuid import uuid4
-
-from sqldiff.appdata import schemas, models
-from sqldiff.appdata.url_template import JdbcUrlTemplate
 
 
 class TestBaseDriverModel(unittest.TestCase):
 
     def test_create_model_from_base_driver_schema(self):
-        # driver_file = DriverFile.construct(
-        #     driver_id=uuid4(),
-        #     file_path=Path('/')
-        # )
+        pass
 
-        driver_file_dict = {
-            'name': 'name',
-            'icon_file_path': Path('/'),
-            'logo_file_path': Path('/'),
-            'dummy': 123
-        }
-
-        driver_file_create = schemas.DriverType(**driver_file_dict)
-        a = 1
-
-    def test_create_driver_file_model_from_schema(self):
-        driver_schema = schemas.DriverFile.construct(driver_id=uuid4(),
-                   file_path=Path('/'))
-
-        driver_file_model_instance = models.DriverFile.create_from_schema(driver_schema, schemas.DriverFile)
-        a = 1
-        x = []
-        b = 1
-
-    def test3(self):
-        driver_create = schemas.DriverCreate(
-            default_port=5432,
-
-        )
--- tests/db/test_jdbc.py
+++ tests/db/test_jdbc.py
@@ -6,87 +6,87 @@ from sqldiff.db.jdbc import JDBCResultDescription
 
 
 class TestJDBCResult(unittest.TestCase):
-
-    def setUp(self):
-        """DB connection used in tests"""
-        # TODO use JDBC mock, it's more about API than data
-        pass
-        self.con = jdbc_api.connect("org.postgresql.Driver",
-                                    "jdbc:postgresql://localhost:5432/postgres",
-                                    ["admin", "admin"],
-                                    "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
-
-    def tearDown(self) -> None:
-        pass
-        # self.con.close()
-
-    def test_create_jdbc_result_description_by_query_result(self):
-        """Test fetching metadata from executed query, using cursor"""
-        query = "select * from test1"
-        cur = self.con.cursor()
-        cur.execute(query)
-        result_description = JDBCResultDescription(cur)
-        desc = cur.description
-        a = 1
-
-    def test_create_jdbc_result_description_by_prepare_statement(self):
-        """Test fetching metadata without query execution"""
-        query = "select * from test1"
-        cur = self.con.cursor()
-        result_description = JDBCResultDescription(cur, query)
-        a = 1
-
-    def test_con(self):
-        query = "select * from test1"
-        cur = self.con.cursor()
-        jconn = cur._connection.jconn
-        db_meta = jconn.getMetaData()
-        d = {
-            'product_name': db_meta.getDatabaseProductName(),
-            'product_version': db_meta.getDatabaseProductVersion(),
-            'driver_name': db_meta.getDriverName(),
-            'driver_version': db_meta.getDriverVersion(),
-        }
-        a = 1
-
-
-class TestDriverErrors(unittest.TestCase):
-    def test_wrong_class_name(self):
-        try:
-            jdbc_api.connect("org.postgresql.Drivere",
-                             "jdbc:postgresql://localhost:5432/postgres",
-                             ["admin", "admin"],
-                             "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
-        except Exception as ex:
-            a = 1
-
-    def test_wrong_url(self):
-        try:
-            jdbc_api.connect("org.postgresql.Driver",
-                             "jdbc:postgresql://localhost_bad:5432/postgres",
-                             ["admin", "admin"],
-                             "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
-        except Exception as ex:
-            a = 1
-
-    def test_wrong_patameters(self):
-        try:
-            jdbc_api.connect("org.postgresql.Driver",
-                             "jdbc:postgresql://localhost:5432/postgres",
-                             ["admin2", "admin2"],
-                             "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
-        except Exception as ex:
-            a = 1
-
-    def test_wrong_jdbc_path(self):
-        try:
-            jdbc_api.connect("org.postgresql.Driver",
-                             "jdbc:postgresql://localhost_bad:5432/postgres",
-                             ["admin", "admin"],
-                             "/home/mateusz/Dev/Db/drivers/postgresqlee/postgresql-42.2.19.jar")
-        except Exception as ex:
-            print(ex)
-            a = 1
+    pass
+#     def setUp(self):
+#         """DB connection used in tests"""
+#         # TODO use JDBC mock, it's more about API than data
+#         pass
+#         self.con = jdbc_api.connect("org.postgresql.Driver",
+#                                     "jdbc:postgresql://localhost:5432/postgres",
+#                                     ["admin", "admin"],
+#                                     "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
+#
+#     def tearDown(self) -> None:
+#         pass
+#         # self.con.close()
+#
+#     def test_create_jdbc_result_description_by_query_result(self):
+#         """Test fetching metadata from executed query, using cursor"""
+#         query = "select * from test1"
+#         cur = self.con.cursor()
+#         cur.execute(query)
+#         result_description = JDBCResultDescription(cur)
+#         desc = cur.description
+#         a = 1
+#
+#     def test_create_jdbc_result_description_by_prepare_statement(self):
+#         """Test fetching metadata without query execution"""
+#         query = "select * from test1"
+#         cur = self.con.cursor()
+#         result_description = JDBCResultDescription(cur, query)
+#         a = 1
+#
+#     def test_con(self):
+#         query = "select * from test1"
+#         cur = self.con.cursor()
+#         jconn = cur._connection.jconn
+#         db_meta = jconn.getMetaData()
+#         d = {
+#             'product_name': db_meta.getDatabaseProductName(),
+#             'product_version': db_meta.getDatabaseProductVersion(),
+#             'driver_name': db_meta.getDriverName(),
+#             'driver_version': db_meta.getDriverVersion(),
+#         }
+#         a = 1
+#
+#
+# class TestDriverErrors(unittest.TestCase):
+#     def test_wrong_class_name(self):
+#         try:
+#             jdbc_api.connect("org.postgresql.Drivere",
+#                              "jdbc:postgresql://localhost:5432/postgres",
+#                              ["admin", "admin"],
+#                              "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
+#         except Exception as ex:
+#             a = 1
+#
+#     def test_wrong_url(self):
+#         try:
+#             jdbc_api.connect("org.postgresql.Driver",
+#                              "jdbc:postgresql://localhost_bad:5432/postgres",
+#                              ["admin", "admin"],
+#                              "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
+#         except Exception as ex:
+#             a = 1
+#
+#     def test_wrong_patameters(self):
+#         try:
+#             jdbc_api.connect("org.postgresql.Driver",
+#                              "jdbc:postgresql://localhost:5432/postgres",
+#                              ["admin2", "admin2"],
+#                              "/home/mateusz/Dev/Db/drivers/postgresql/postgresql-42.2.19.jar")
+#         except Exception as ex:
+#             a = 1
+#
+#     def test_wrong_jdbc_path(self):
+#         try:
+#             jdbc_api.connect("org.postgresql.Driver",
+#                              "jdbc:postgresql://localhost_bad:5432/postgres",
+#                              ["admin", "admin"],
+#                              "/home/mateusz/Dev/Db/drivers/postgresqlee/postgresql-42.2.19.jar")
+#         except Exception as ex:
+#             print(ex)
+#             a = 1
 
 
 
